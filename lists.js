fa829b5e37bce3c416a1290bea5d19ab79e01a6d
Kevin Loughead <kvnloughead@gmail.com>
2020-01-12 11:31:02 -0500

    Refactored List into a class

--- lists.js
+++ lists.js
@@ -4,136 +4,116 @@
     // make insert method allow insertion of multiple elements
     //                and allow locating insertion by index
 
-function List(){
-    this.dataStore = [];
-    this.listSize = 0;
-    this.pos = 0;                    
-    this.append = append;
-    this.find = find;
-    this.remove = remove;
-    this.length = length;               // getter for listSize
-    this.clear = clear;               
-    this.toString = toString;       
-    this.getElement = getElement;      // gets element at current position
-    this.getPos = getPos;         
-    this.setPos = setPos; 
-    this.contains = contains;
-    this.insert = insert;
-    this.front = front;             // set this.pos = 0
-    this.end = end;                 // set this.pos = this.length - 1
-    this.previous = previous;       // Next four are the iterators
-    this.next = next;
-    this.hasPrevious = hasPrevious;
-    this.hasNext = hasNext;
-              
-}
-
-function append(element){
-    this.dataStore[this.listSize++] = element;
-};
-
-function find(element){
-    for (i = 0; i < this.dataStore.length; i++){
-        if (this.dataStore[i] === element){
-            return i;
-        }
+class List {
+    constructor() {
+        this.dataStore = [];
+        this.listSize = 0;
+        this.pos = 0;  
     };
-    return -1;
-};
-
-function remove(element){
-    //removes element, shifting indices for remaining indices as needed
-    var foundAt = this.find(element);
-    if (foundAt > -1){
-        this.dataStore.splice(foundAt, 1);
-        --this.listSize;
+    append(element){
+        this.dataStore[this.listSize++] = element;
+    };
+    find(element){
+        for (var i = 0; i < this.dataStore.length; i++){
+            if (this.dataStore[i] === element){
+                return i;
+            }
+        };
+        return -1;
+    };
+    remove(element){
+        //removes element, shifting indices for remaining indices as needed
+        var foundAt = this.find(element);
+        if (foundAt > -1){
+            this.dataStore.splice(foundAt, 1);
+            --this.listSize;
+            return true;
+        }
+        return false;
+    };
+    insert(newElement, afterThis=undefined, byIndex=false){
+        // inserts newElement after first instance of afterThis (if possible)
+        // leaving afterThis=undefined inserts at front of list.
+        // byIndex=true makes afterThis an index.
+        if (afterThis === undefined){
+            this.dataStore.splice(0, 0, newElement)
+            return true;
+        }
+        if (byIndex){
+            var insertPos = afterThis;
+        } else {
+            var insertPos = this.find(afterThis);
+        }
+        if (insertPos != -1){
+            this.dataStore.splice(insertPos+1, 0, newElement);
+            ++this.listSize;
+            return true;
+        }
+        return false;
+    };
+    length(){
+        return this.listSize;
+    }
+    clear(){
+        delete this.dataStore;  // not sure why delete is necessary
+        this.dataStore = [];
+        this.listSize = this.pos = 0;
+    }
+    toString(){
+        return this.dataStore;
+    }
+    contains(element){
+        if (this.find(element) === -1){
+            return false;
+        }
         return true;
     }
-    return false;
-}
-
-function insert(newElement, afterThisElement){
-    // inserts newElement afterThisElement (if possible)
-
-    var insertPos = this.find(afterThisElement);
-    if (insertPos != -1){
-        this.dataStore.splice(insertPos+1, 0, newElement);
-        ++this.listSize;
+    getElement(){
+        return this.dataStore[this.pos];
+    }
+    setPos(position){
+        this.pos = position;
+    }
+    getPos(position){
+        return this.pos;
+    }
+    previous(){
+        // returns undefined if not hasPrevious
+        //   Possibly should change this behavior?
+        var thisOne = this.dataStore[this.pos];
+        --this.pos;
+        return thisOne;
+    }
+    next(){
+        // returns undefined if not hasNext
+        //   Possibly should change this behavior?
+        var thisOne = this.dataStore[this.pos];
+        ++this.pos;
+        return thisOne;
+    }
+    hasNext(){
+        if (this.pos > this.listSize - 1) {
+            return false;
+        }
         return true;
     }
-    return false;
-}
-
-function length(){
-    return this.listSize;
-}
-
-function clear(){
-    delete this.dataStore;  // not sure why delete is necessary
-    this.dataStore = [];
-    this.listSize = this.pos = 0;
-}
-
-function toString(){
-    return this.dataStore;
-}
-
-function contains(element){
-    if (this.find(element) === -1){
-        return false;
+    hasPrevious(){
+        if (this.pos <= 0) {
+            return false;
+        }
+        return true;
     }
-    return true;
-}
-
-function getElement(element){
-    return this.dataStore[this.pos];
-}
-
-function setPos(position){
-    this.pos = position;
-}
-
-function getPos(position){
-    return this.pos;
-}
-
-function previous(){
-    // returns undefined if not hasPrevious
-    thisOne = this.dataStore[this.pos];
-    --this.pos;
-    return thisOne;
-}
-
-function next(){
-    // returns undefined if not hasNext
-    thisOne = this.dataStore[this.pos];
-    ++this.pos;
-    return thisOne;
-}
-
-function hasNext(){
-    if (this.pos > this.listSize - 1) {
-        return false;
+    front(){
+        this.pos = 0;
     }
-    return true;
-}
-
-function hasPrevious(){
-    if (this.pos <= 0) {
-        return false;
+    end(){
+        this.pos = this.listSize - 1;
     }
-    return true;
-}
-
-function front(){
-    this.pos = 0;
-}
-
-function end(){
-    this.pos = this.listSize - 1;
 }
 
 function test(){
+    // Testing functions
+    // Not very robust or complete
     var numbers = new List()
     numbers.append(1)
     numbers.append(2)
